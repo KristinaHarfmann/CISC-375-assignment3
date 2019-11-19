9ffa084a7bff9e5c9b5b98ffc291d8a3f75694af
Kharfmann <33815851+Kharfmann@users.noreply.github.com>
2019-11-19 17:51:52 -0600

    almost finished incidents

--- server.js
+++ server.js
@@ -83,8 +83,6 @@ app.get('/neighborhoods', (req, res) => {
 			{
 				if(ids.includes(rows[i].neighborhood_number.toString()))
 				{
-					//console.log("list ids = " + ids[i]);
-					//console.log("rows = " + rows[j].neighborhood_number);
 
 					var newNeigh = "N" + rows[i].neighborhood_number;
 					data[newNeigh] = rows[i].neighborhood_name;
@@ -114,12 +112,246 @@ app.get('/neighborhoods', (req, res) => {
 });
 
 app.get('/incidents', (req, res) => {
+		
+	var data = {};
+	var format = req.query.format;
+	var setId = "";
+	var setCode = "";
+	var setGrid = "";
+
+
+
+	var dbString = "SELECT * FROM Incidents";
+	//limit, end_date, start_date, id, code, grid
+	
+	
+	if(req.query.id != undefined)
+	{//set up ids
+		var ids = req.query.id.split(",");
+		var setId = "( neighborhood_number = " + ids[0];
+		for(i = 1; i < ids.length; i++)
+		{
+			setId = setId + " OR neighborhood_number = " + ids[i];
+		}
+		setId = setId + ")";
+	}
+	
+	if(req.query.code != undefined)
+	{//set up codes
+		var codes = req.query.code.split(",");
+		setCode = "( code = " + codes[0];
+		for(i = 1; i < codes.length; i++)
+		{
+			setCode = setCode + " OR code = " + codes[i];
+		}
+		setCode = setCode + ")";
+	}
+	
+	if(req.query.grid != undefined)
+	{//set up grids
+		var grids = req.query.grid.split(",");
+		setGrid = "( police_grid = " + grids[0];
+		for(i = 1; i < grids.length; i++)
+		{
+			setGrid = setGrid + " OR police_grid = " + grids[i];
+		}
+		setGrid = setGrid + ")";
+	}
+	
+	
+	
+	if(req.query.end_date != undefined)
+	{//put in end date
+		dbString = dbString + " WHERE date_time <= " + req.query.end_date;
+	}
+	
+	if(req.query.start_date != undefined && req.query.end_date != undefined)
+	{//put in start date
+		dbString = dbString + " AND date_time >= " + req.query.start_date;
+	}
+	else if(req.query.start_date != undefined && req.query.end_date == undefined)
+	{
+		dbString = dbString + " WHERE date_time >= " + req.query.start_date;
+	}
+	
+	
+	if(req.query.id != undefined && (req.query.end_date != undefined || req.query.start_date != undefined))
+	{//put in ids
+
+		dbString = dbString + " AND " + setId;
+	}
+	else if(req.query.id != undefined && (req.query.end_date == undefined && req.query.start_date == undefined))
+	{
+		dbString = dbString + " WHERE " + setId;
+	}
+	
+	
+	if(req.query.code != undefined && (req.query.end_date != undefined || req.query.start_date != undefined || req.query.id != undefined))
+	{//put in codes
+		dbString = dbString + " AND " + setCode;
+	}
+	else if(req.query.code != undefined && (req.query.end_date == undefined && req.query.start_date == undefined && req.query.id == undefined))
+	{
+		dbString = dbString + " WHERE " + setCode;
+	}
+	
+	if(req.query.grid != undefined && (req.query.end_date != undefined || req.query.start_date != undefined || req.query.id != undefined || req.query.grid != undefined))
+	{//put in grids
+		dbString = dbString + "AND " + setGrid;
+	}
+	else if(req.query.grid != undefined && (req.query.end_date == undefined && req.query.start_date == undefined && req.query.id == undefined && req.query.grid == undefined))
+	{
+		dbString = dbString + " WHERE " + setGrid;
+	}
+	
+	dbString = dbString + " ORDER BY date_time DESC";
+	
+	
+	if(req.query.limit != undefined)
+	{//put in limit
+		dbString = dbString + " LIMIT " + req.query.limit;
+	}
+	else if(req.query.limit == undefined)
+	{
+		dbString = dbString + " LIMIT 10000";
+	}
+	
+	
+	
+	console.log(dbString);
+	
+	
+	db.all(dbString, (err, rows) => {
+		
+		console.log(rows.length);
+		for (i = 0; i < rows.length; i++)
+		{
+			var newIncident = "I" + rows[i].case_number;
+			var newDate = rows[i].date_time.substring(0, 9);
+			var newTime = rows[i].date_time.substring(11,19);
+			
+			console.log(rows[i].date_time);
+			
+			data[newIncident] = 
+			{ 
+				date : newDate,
+				time : newTime,
+				code : rows[i].code, 
+				incident : rows[i].incident, 
+				police_grid : rows[i].police_grid, 
+				neighborhood_number : rows[i].neighborhood_number, 
+				block : rows[i].block
+			};
+		}//for
+		
+		if(format == 'xml')
+		{
+			var xml = js2xmlparser.parse("incidents", data);
+			res.type('xml').send(xml);
+		}
+		else
+		{
+			res.type('json').send(data);
+		}
+			
+	});
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	//db call for each combination of options
+	
+	
+	//no input
+	/*if(req.query.limit == undefined && req.query.end_date == undefined && req.query.start_date == undefined && req.query.id == undefined && req.query.code == undefined && req.query.grid == undefined)
+	{
+		
+		db.all("SELECT top ? * Incidents ORDER BY date_time desc", 10000, (err, rows) => {
+			
+				
+			for (i = 0; i < rows.length; i++)
+			{
+				var newDate = rows[i].date_time.substring(0, 9);
+				var newTime = rows[i].date_time.substring(11);
+				
+				data[newIncident] = 
+				{ 
+					date : newDate,
+					time : newTime,
+					code : rows[i].code, 
+					incident : rows[i].incident, 
+					police_grid : rows[i].police_grid, 
+					neighborhood_number : rows[i].neighborhood_number, 
+					block : rows[i].block
+				};
+			}//for
+			
+		});//db.all
+	}
+	
+	//only limit input
+	if(req.query.limit != undefined && req.query.end_date == undefined && req.query.start_date == undefined && req.query.id == undefined && req.query.code == undefined && req.query.grid == undefined)
+	{
+		db.all("SELECT * Incidents WHERE limit = ? ORDER BY date_time desc", req.query.limit, (err, rows) => {
+			
+		});
+	}
+	
+	//only end_date input
+	if(req.query.limit == undefined && req.query.end_date != undefined && req.query.start_date == undefined && req.query.id == undefined && req.query.code == undefined && req.query.grid == undefined)
+	{
+		db.all("SELECT * Incidents WHERE date_time <= ? ORDER BY date_time desc", req.query.end_date, (err, rows) => {
+			
+		});
+	}
+	
+	//only start_date input
+	if(req.query.limit == undefined && req.query.end_date == undefined && req.query.start_date != undefined && req.query.id == undefined && req.query.code == undefined && req.query.grid == undefined)
+	{
+		db.all("SELECT * Incidents WHERE date_time = ? ORDER BY date_time desc", req.query.start_date, (err, rows) => {
+			
+		});
+	}
+
+	//only id input
+	if(req.query.limit == undefined && req.query.end_date == undefined && req.query.start_date == undefined && req.query.id != undefined && req.query.code == undefined && req.query.grid == undefined)
+	{
+		db.all("SELECT * Incidents WHERE neighborhood_number = ? ORDER BY date_time desc", req.query.id, (err, rows) => {
+			
+		});
+	}
+
+	//only code input
+	if(req.query.limit == undefined && req.query.end_date == undefined && req.query.start_date == undefined && req.query.id == undefined && req.query.code != undefined && req.query.grid == undefined)
+	{
+		db.all("SELECT * Incidents WHERE code = ? ORDER BY date_time desc", req.query.code, (err, rows) => {
+			
+		});
+	}
+
+	//only grid input
+	if(req.query.limit == undefined && req.query.end_date == undefined && req.query.start_date == undefined && req.query.id == undefined && req.query.code == undefined && req.query.grid != undefined)
+	{
+		db.all("SELECT * Incidents WHERE police_grid = ? ORDER BY date_time desc", req.query.grid, (err, rows) => {
+			
+		});
+	}	
+	
+	
+	
+	
+	
+	
+	
+	
 	db.all("SELECT * FROM Incidents ORDER BY date_time desc", (err, rows) => {
-		var data = {};
-		var endDate = "2019-12-30";
-		var startDate = "2014-08-1";
-		var limit = 10000;
-		var format = req.query.format;
+
 		
 		if(req.query.limit != undefined){
 			limit = req.query.limit;
@@ -160,11 +392,12 @@ app.get('/incidents', (req, res) => {
 					};
 				}
 			}
+		}
 					
 					
 			
 
-		/*for (i = 0; i < rows.length; i++)
+		for (i = 0; i < rows.length; i++)
 		{
 			var newIncident = "I" + rows[i].case_number;
 			var newDate = rows[i].date_time.substring(0, 9);
@@ -179,20 +412,11 @@ app.get('/incidents', (req, res) => {
 				neighborhood_number : rows[i].neighborhood_number, 
 				block : rows[i].block
 			};
-		}*/
-		
-		
-		
-		if(format == 'xml')
-		{
-			var xml = js2xmlparser.parse("incidents", data);
-			res.type('xml').send(xml);
 		}
-		else
-		{
-			res.type('json').send(data);
-		}
-	});//db.all
+		
+	*/
+	
+	//});//db.all
 });
 
 app.put('/new-incident', (req, res) => {
